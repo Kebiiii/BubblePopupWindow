81d99fca52a47efd0e9042875b40268e83120151
wdevid <921126wyb>
2018-10-13 09:48:08 +0800

    Initial commit

---
+++ app/src/main/java/com/kebiiii/bubble/MainActivity.java
@@ -0,0 +1,73 @@
+package com.kebiiii.bubble;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import com.kebiiii.bubblepopup.BubblePopupWindow;
+
+
+/**
+ * Instruction：
+ * Author：Kebii
+ * Email：liukebing@foxmail.com
+ * 2018/10/11 19:19
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private BubblePopupWindow leftTopWindow;
+    private BubblePopupWindow rightTopWindow;
+    private BubblePopupWindow leftBottomWindow;
+    private BubblePopupWindow rightBottomWindow;
+    private BubblePopupWindow centerWindow;
+
+    LayoutInflater inflater;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        leftTopWindow = new BubblePopupWindow(MainActivity.this);
+        rightTopWindow = new BubblePopupWindow(MainActivity.this);
+        leftBottomWindow = new BubblePopupWindow(MainActivity.this);
+        rightBottomWindow = new BubblePopupWindow(MainActivity.this);
+        centerWindow = new BubblePopupWindow(MainActivity.this);
+
+        inflater = LayoutInflater.from(this);
+    }
+
+    public void leftTop(View view) {
+        View bubbleView = inflater.inflate(R.layout.layout_popup_view, null);
+        TextView tvContent = (TextView) bubbleView.findViewById(R.id.tvContent);
+        tvContent.setText("HelloWorld");
+        leftTopWindow.setBubbleView(bubbleView);
+        leftTopWindow.show(view, Gravity.BOTTOM,0,50,0);
+    }
+
+    public void rightTop(View view) {
+        View bubbleView = inflater.inflate(R.layout.layout_popup_view, null);
+        rightTopWindow.setBubbleView(bubbleView);
+        rightTopWindow.show(view, Gravity.LEFT, 0,0,50);
+    }
+
+    public void leftBottom(View view) {
+        View bubbleView = inflater.inflate(R.layout.layout_popup_view, null);
+        leftBottomWindow.setBubbleView(bubbleView);
+        leftBottomWindow.show(view);
+    }
+
+    public void rightBottom(View view) {
+        View bubbleView = inflater.inflate(R.layout.layout_popup_view, null);
+        rightBottomWindow.setBubbleView(bubbleView);
+        rightBottomWindow.show(view, Gravity.RIGHT, 0,0,50);
+    }
+
+    public void center(View view) {
+        View bubbleView = inflater.inflate(R.layout.layout_popup_view, null);
+        centerWindow.setBubbleView(bubbleView);
+        centerWindow.show(view, Gravity.BOTTOM, 0);
+    }
+}
